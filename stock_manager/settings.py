453191e1468222b1252aa200196211d4b7f05b48
HESSACK-R <hessackryansteeve@gmail.com>
2025-11-01 19:01:06 +0100

    Up to date

--- stock_manager/settings.py
+++ stock_manager/settings.py
@@ -5,7 +5,7 @@ import dj_database_url
 from decouple import Config, Csv
 
 # Crée une instance de Config sans 'search_path'
-config = Config()
+config = Config(search_path='.env')  
 
 # Base Directory
 BASE_DIR = Path(__file__).resolve().parent.parent
