ef43df3e4dc913511b3aeb3750456ad7564bc01a
HESSACK-R <hessackryansteeve@gmail.com>
2025-11-02 09:24:53 +0100

    Add Django TEMPLATES configuration

--- stock_manager/settings.py
+++ stock_manager/settings.py
@@ -41,6 +41,24 @@ MIDDLEWARE = [
 # URLs
 ROOT_URLCONF = 'stock_manager.urls'
 
+# Templates settings
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [BASE_DIR / 'templates'],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
+
 # WSGI application
 WSGI_APPLICATION = 'stock_manager.wsgi.application'
 
